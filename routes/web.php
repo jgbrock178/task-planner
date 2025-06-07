5555694188274e062fa6c6b76bef374ff967082f
John Brock <jgbrock178@gmail.com>
2025-06-07 18:21:30 +0100

    🚚 Add /tasks route and cleanup middleware

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,15 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::get('tasks', function () {
+        return Inertia::render('Tasks');
+    })->name('tasks');
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
